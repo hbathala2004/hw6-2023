9b2f42ac71e1a531cc95ef6cb97841d6dc554efc
hbathala2004 <156857050+hbathala2004@users.noreply.github.com>
2024-04-02 20:10:22 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,10 +4,11 @@ var video = document.querySelector ("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video=document.querySelector("#player1")
 	video.autoplay = false;
-	video.load ();
-	video.pause ();
 	video.loop = false; 
+	console.log("Auto play is set to "+ video.autoplay)
+	console.log("loop is set to"+ video.loop)
 	video.volume = document.querySelector ("#slider").value /100;
 
 });
@@ -15,22 +16,37 @@ window.addEventListener("load", function() {
 
 document.querySelector("#pause").addEventListener("click", function() {
 video.pause ();
-console.log ("Pause video");
+console.log ("pause video");
 });
 document.querySelector("#play").addEventListener("click", function() {
-console.log("Play Video");
-console.log("Play Video");
+console.log("play video");
 video.play ();
 document.querySelector ("#volume").textContent = Math.round (video.volume * 100) + "%";
 
 document.querySelector("#slower").addEventListener("click", function() {
-video.playbackRate -= 0.1; 
-console.log ("Slow Down");
+video.playbackRate -= 0.10; 
+console.log ("slow down");
+});
+
+
+document.querySelector("#play").addEventListener("click", function() {
+	video.play()
+	video.volume = (document.querySelector("#slider").value/100)
+	document.querySelector("#volume").innerHTML = video.volume*100 + "%"
+
+	document.querySelector("#faster").addEventListener("click", function() {
+		video.playbackRate += .10
+		console.log("go faster")
+
+	document.querySelector("#skip").addEventListener("click", function() {
+		video.currentTime += 0.1; 
+		if (video.currentTime < video.duration) {
+		video.currentTime = 0; 
+			}
+			console.log ("Skip Ahead");
+			console.log("New Time " + video.currentTime);
 });
 
 
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
 
