f6bf82f3c8e19887b331b675cc143f4854aa8b52
hbathala2004 <156857050+hbathala2004@users.noreply.github.com>
2024-04-04 00:21:01 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,16 +1,15 @@
 var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
-    console.log("Good job opening the window")
-    video = document.querySelector("#player1")
+    console.log("Good job opening the window");
+    video = document.querySelector("#player1");
     video.autoplay = false;
     video.loop = false;
-    console.log("Auto play is set to " + video.autoplay)
-    console.log("loop is set to" + video.loop)
+    console.log("Auto play is set to " + video.autoplay);
+    console.log("loop is set to " + video.loop);
     video.volume = document.querySelector("#slider").value / 100;
 
 });
-
 
 document.querySelector("#pause").addEventListener("click", function() {
     video.pause();
@@ -40,4 +39,23 @@ document.querySelector("#skip").addEventListener("click", function() {
     }
     console.log("Skip Ahead");
     console.log("New Time " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted == false){
+        video.muted = true;
+        document.getElementById('volume').innerHTML = 'muted';
+        document.getElementById('mute').innerHTML = 'Unmute';
+    } else {
+        video.muted = false;
+        document.getElementById('volume').innerHTML = video.volume * 100 + '%';
+        document.getElementById('mute').innerHTML = 'Mute';
+    }
+
+    document.querySelector("#slider").addEventListener("click", function() {
+        var volume = volumeslider.value;
+        video.volume = volume / 100;
+        document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
+    
+    });
 });
